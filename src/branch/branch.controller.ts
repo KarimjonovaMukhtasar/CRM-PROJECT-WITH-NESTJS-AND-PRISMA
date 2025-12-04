d34532a7c573bd81beea3c5e1af6f730d22e063f
KarimjonovaMukhtasar <karimjonovamukhtasar2003@gmail.com>
2025-12-04 14:13:05 +0500

    feature: branch table validations CRUD created and tested successfully

---
+++ src/branch/branch.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { BranchService } from './branch.service';
+import { CreateBranchDto } from './dto/create-branch.dto';
+import { UpdateBranchDto } from './dto/update-branch.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Controller('branch')
+export class BranchController {
+  constructor(private readonly branchService: BranchService) {}
+
+  @Post()
+  create(@Body() createBranchDto: CreateBranchDto) {
+    return this.branchService.create(createBranchDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.branchService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.branchService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateBranchDto: UpdateBranchDto) {
+    return this.branchService.update(id, updateBranchDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string, @Query('force') force?: string) {
+    return this.branchService.remove(id, force === 'true');
+  }
+}
