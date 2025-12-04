07a6cc6de81eb090fe2856ecb0e356c44696dcad
KarimjonovaMukhtasar <karimjonovamukhtasar2003@gmail.com>
2025-12-04 14:45:54 +0500

    feature: ROOM controller validation service CRUD added and tested successfully

--- src/room/dto/create-room.dto.ts
+++ src/room/dto/create-room.dto.ts
@@ -1,1 +1,16 @@
-export class CreateRoomDto {}
+import { IsNotEmpty, IsNumber, IsPositive, IsString, IsUUID } from 'class-validator';
+
+export class CreateRoomDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
+  capacity: number;
+
+  @IsUUID()
+  @IsNotEmpty()
+  branchId: string;
+}
